8af5ec5237a9abebec7cce25b2a44f8f8391eab3
mettemfurfur000 <tmettem@gmail.com>
2023-03-26 22:40:22 +0500

    fix wrong data saving&loading

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ using namespace std;
 int main()
 {
     read_file_and_print("logo.txt");
-    load_arrows(arrows4,4);
+    load_arrows(arrows4, 4);
     int delay = (int)(1000 / cps);
-    while(!close_app_NOW)
+    while (!close_app_NOW)
     {
         check_buttons();
-        if(work) scan_and_do(arrows4,possible_gap,delay_press,delay_release);
+        if (work)            scan_and_do(arrows4, possible_gap, delay_press, delay_release);
         Sleep(delay);
     }
-    save_arrows(arrows4,4);
+    save_arrows(arrows4, 4);
 }
