cd3f2870362b2876ef1625e5ebdb9802ae601ae1
mettemfurfur000 <tmettem@gmail.com>
2023-03-27 13:54:55 +0500

    80%

--- main.cpp
+++ main.cpp
@@ -7,12 +7,13 @@ int main()
     read_file_and_print("logo.txt");
     load_arrows(arrows4, 4);
     int delay = (int)(1000 / cps);
+
     while (!close_app_NOW)
     {
         check_buttons();
         if (work)
             scan_and_do(arrows4, possible_gap, delay_press, delay_release);
-       Sleep(delay);
+        Sleep(delay);
     }
     save_arrows(arrows4, 4);
     return 0;
